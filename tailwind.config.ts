f64058170717ca835ab28408f0508f66178136b0
Lê Đức Dũng <l.dung.0698@gmail.com>
2025-03-24 00:55:43 -0400

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF0000',
+        secondary: '#282828',
+        dark: '#0F0F0F',
+        light: '#FFFFFF',
+        gray: {
+          100: '#F2F2F2',
+          200: '#E5E5E5',
+          300: '#D4D4D4',
+          400: '#A3A3A3',
+          500: '#737373',
+          600: '#525252',
+          700: '#404040',
+          800: '#262626',
+          900: '#171717',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config 
